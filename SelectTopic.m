56b3575eacfe8c4ff4b0f294efc69ef8a2a38402
Joseph caxton-Idowu <Joseph@Joseph-caxton-Idowus-iMac.local>
2011-10-30 11:40:31 +0000

    all problems

--- SelectTopic.m
+++ SelectTopic.m
@@ -131,7 +131,7 @@
     
 	EvaluatorAppDelegate *appDelegate = (EvaluatorAppDelegate *)[UIApplication sharedApplication].delegate;
 	
-	if (indexPath.row > [[fetchedResultsController fetchedObjects]count] -1) {
+	if (indexPath.row == 0) {  //indexPath.row > [[fetchedResultsController fetchedObjects]count] -1
 		
 		cell.textLabel.text = @"All";
 		
@@ -147,8 +147,9 @@
 	}
 	else{
 		
+        NSInteger val = indexPath.row;
 		
-	Topics *T = (Topics *)[fetchedResultsController objectAtIndexPath:indexPath];
+	Topics *T = (Topics *)[[fetchedResultsController fetchedObjects] objectAtIndex:val - 1];
 	cell.textLabel.text = T.TopicName;
 		
 		if ([appDelegate.Topic  isEqualToString:T.TopicName ]) {
@@ -225,16 +226,20 @@
 	
 	if (UserConfigure) {
 		EvaluatorAppDelegate *appDelegate = (EvaluatorAppDelegate *)[UIApplication sharedApplication].delegate;
-		if (indexPath.row > [[fetchedResultsController fetchedObjects]count] -1) {
+		if (indexPath.row ==0) { //indexPath.row > [[fetchedResultsController fetchedObjects]count] -1
 			appDelegate.Topic = @"All";
 			[self.navigationController popViewControllerAnimated:TRUE];
 		}
 		else {
-			SelectedTopic = (Topics *)[fetchedResultsController objectAtIndexPath:indexPath];
+            
+             NSInteger val = indexPath.row;
+			SelectedTopic = (Topics *)[[fetchedResultsController fetchedObjects] objectAtIndex:val - 1];
 			appDelegate.Topic = SelectedTopic.TopicName;
 			[self.navigationController popViewControllerAnimated:TRUE];
 		}
 		
+        //NSLog(@"%@", appDelegate.Topic);
+        
 	}
 	
 	
