cea6e1537325e6051a7b3959d7e718bf01ebea63
Joseph caxton-Idowu <Joseph@Joseph-caxton-Idowus-iMac.local>
2012-07-10 10:58:42 +0100

    minor

--- Start.m
+++ Start.m
@@ -662,6 +662,17 @@ if ([self.FirstView superview]) {
         [self reviewPressed];
         
     }
+    else {
+        NSUserDefaults *prefs = [NSUserDefaults standardUserDefaults];
+        NSString *ReviewID = [prefs stringForKey:@"Review"];
+        NSInteger Counter = [ReviewID integerValue];
+        NSInteger CounterPlus = Counter + 1;
+        NSString *ID = [NSString stringWithFormat:@"%d",CounterPlus];
+        [prefs setObject:ID  forKey:@"Review"];
+        [prefs synchronize];
+        
+    }
+
     
 }
 
